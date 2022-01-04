72d71d8b162df3755db52750cc1fe9f5ccba99be
sivertillya <illia.sivert@smartexe.com>
2022-01-04 15:01:21 +0200

    Fix date, update js

--- js/main.js
+++ js/main.js
@@ -1,74 +1,53 @@
-$(document).ready(function() {
-    // Годиник
-	var pos = document.getElementById("time");
+$(document).ready(function () {
+  // Годиник
+  const pos = document.getElementById('time')
 
-	function time() {
-		var today = new Date();
-		var hours_ = today.getHours();
-		var min_ = today.getMinutes();
-		var sec_ = today.getSeconds();
-        var zeroh = "";
-        var zerom =  "";
-        var zeros = "";
-        if(hours_ < 10) zeroh = '0';
-        if(min_ < 10) zerom = '0';
-        if(sec_ < 10) zeros = '0';
-        var nowTime = zeroh + hours_ +':'+ zerom + min_;
-        var displayTime = hours_ +':'+ zerom + min_;
-        // console.log(nowTime);
-        pos.innerHTML = displayTime + ':'+zeros+sec_;
-        return nowTime;
-	}
-	setInterval(time, 1);
+  const time = () => {
+    const today = new Date()
+    let hours_ = today.getHours()
+    let min_ = today.getMinutes()
+    let sec_ = today.getSeconds()
+    hours_ = hours_ < 10 ? '0' + hours_ : hours_
+    min_ = min_ < 10 ? '0' + min_ : min_
+    sec_ = sec_ < 10 ? '0' + sec_ : sec_
+    const nowTime = hours_ + ':' + min_
+    pos.innerHTML = nowTime + ':' + sec_
+    return nowTime
+  }
 
-	var nowTimeHM = time();
+  setInterval(time, 0)
 
-    console.log(nowTimeHM);
+  const nowTimeHM = time()
 
-    // Перебор графика
-    var schedule = [];
+  // Перебор графика
+  const schedule = []
 
-    // debugger
+  const tableItems = document.querySelectorAll('table.so_ts_table')
 
-
-    var tableItems = document.querySelectorAll( "table.so_ts_table" );
-    var dataTimeItems;
-    var currentTable;
-    var counter;
-
-    console.log(tableItems[4]);
-
-    for (var y = 0; y < tableItems.length; y++) {
-        counter = 0;
-        console.log(y);
-        currentTable = tableItems[y];
-        console.log(tableItems[y]);
-
-        dataTimeItems = currentTable.querySelectorAll( 'td[data-time]' );
-
-        for (var i = 0; i < dataTimeItems.length; i++) {
-            var tableTime = dataTimeItems[i].getAttribute("data-time");
-            console.log(nowTimeHM);
-
-            if (tableTime > nowTimeHM) {
-                if (counter < 3 ){
-                    counter++;
-                    dataTimeItems[i].classList.add("mark_time_" + counter);
-                    console.log(dataTimeItems[i].getAttribute("data-time"));
-                }
-            }
+  tableItems.forEach(table => {
+    let counter = 0
+    const dataTimeItems = table.querySelectorAll('td[data-time]')
+    dataTimeItems.forEach(item => {
+      const tableTime = item.getAttribute('data-time')
+      if (tableTime > nowTimeHM) {
+        if (counter < 3) {
+          counter++
+          item.classList.add('mark_time_' + counter)
         }
-    }
+      }
+    })
+  })
 
-    function scrollTo( target ) {
-        if( target.length ) {
-            $("html, body").stop().animate( { scrollTop: target.offset().top }, 1000);
-        }
-    }
 
-    $(".scroll-link").on("click", function(e) {
-        e.preventDefault();
-        var href = $(this).attr('href');
-        scrollTo( $(href) );
-    });
-});
+  const scrollTo = (target) => {
+    if (target.length) {
+      $('html, body').stop().animate({ scrollTop: target.offset().top }, 1000)
+    }
+  }
+
+  $('.scroll-link').on('click', function (e) {
+    e.preventDefault()
+    const href = $(this).attr('href')
+    scrollTo($(href))
+  })
+})
